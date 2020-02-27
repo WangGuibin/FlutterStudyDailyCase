5f194a7379e6f2e8fca9c06c00e632a90c4560df
Wangguibin <864562082@qq.com>
2020-02-27 19:58:15 +0800

    update node.js server

---
+++ node_server/README.md
@@ -0,0 +1,18 @@
+# Node.JS 启动一个WebServer
+
+
+
+### 实现步骤:
+
+####  1.下载安装node环境
+
+####  2. clone本新项目,安装一下node_modules即可开启
+
+```bash
+git clone https://wwww.github.com/WangGuibin/FlutterStudyDailyCase.git
+cd node_server
+npm install
+npm start
+
+```
+
---
+++ node_server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.use(function(req, res, next) {
+    const proxy = req.query.proxy;
+    if (proxy) {
+        req.header.cookie = req.header.cookie + `__proxy__${proxy}`;
+    }
+    next();
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`Node WebServer服务已启动 runing @http://localhost:${port}`);
+});
+
+module.exports = app;
